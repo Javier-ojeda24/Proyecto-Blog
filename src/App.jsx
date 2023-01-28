f63c5b07c0a8f5c693e68320462bdcb63fb02b0c
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-01-28 11:52:36 -0300

    estructura de carpeta

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { Articulos } from "./components/pages/Articulos";
+import { Crear } from "./components/pages/Crear";
+import { Inicio } from "./components/pages/Inicio";
 
 function App() {
   return (
-     <div className="App">
-    
-     </div>
-  )
+    <div className="App">
+      <h1>Blog con React</h1>
+      <Inicio />
+      <Articulos />
+      <Crear />
+    </div>
+  );
 }
 
 export default App;
