833341e97b859883e1d86c134f70df6cb1c3a40d
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-07 12:12:29 -0300

    sacando app de arranque

--- arrancar-proyecto.bat
+++
@@ -1,3 +0,0 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\6.0\bin && mongod.exe"
-start cmd.exe /k "cd C:\Users\Javier\Desktop\Proyectos\Api Node js && npm start"
-start cmd.exe /k "cd C:\Users\Javier\Desktop\Proyectos\15-Proyecto3 && npm run dev"
