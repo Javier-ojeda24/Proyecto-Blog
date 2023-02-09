a679a07ac638c980e449fc10ea656b097a551ea9
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-02-09 11:03:04 -0300

    guardando articulos

--- src/components/pages/Crear.jsx
+++ src/components/pages/Crear.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { useState } from "react";
 import { useForm } from "../../hooks/useform";
+import { Peticion } from "../../helpers/Peticion";
+import { Global } from "../../helpers/Global";
 
 export const Crear = () => {
   const { formulario, enviado, cambiado } = useForm({});
-  const guardarArticulo = (e) => {
-    e.preventDefaul();
+  const [resultado, setResultado] = useState("no_enviado");
+  const guardarArticulo = async (e) => {
+    e.preventDefault();
     //Recoger ddatos del formulario
-    let nuevoArticulo = JSON.stringify(formulario);
+    let nuevoArticulo = formulario;
 
     //Guardar articulo en el back
-    
+    const { datos, cargando } = await Peticion(
+      Global.url + "crear",
+      "POST",
+      nuevoArticulo
+    );
+    if (datos.status === "success") {
+      setResultado("guardado");
+    } else {
+      setResultado("error");
+    }
+    // console.log(datos);
   };
 
   return (
     <div className="jumbo">
       <h1>Crear articulo</h1>
       <p>Formulario para crear un articulo</p>
-      <pre>{JSON.stringify(formulario)}</pre>
+
+      <strong>
+        {resultado == "error" ? "Los datos proporcionados con incorrectos" : ""}
+      </strong>
       {/* Creando Formulario */}
       <form className="formulario" onSubmit={guardarArticulo}>
         <div className="form-group">
